164e4fd599db57430fec41523f1e8cf02a49f898
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-08-01 00:43:12 +0200

    install dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
+// Load env vars
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 5000;
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
 
 export default app;
